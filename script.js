bbc310e65947ce0dd8abba8f9e6a4f976b73084a
Pedro Lucas <79203933+pedrowlucas@users.noreply.github.com>
2022-07-26 20:57:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function limpar(){
+    var nome = document.getElementsByName('nome')
+    var email = document.getElementsByName('email')
+    var senha = document.getElementsByName('senha')
+    var confSenha = document.getElementsByName('confSenha')
+}
