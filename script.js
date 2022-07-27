26410701eaeb7dd16a82615952b0eab14a523ca0
Pedro Lucas <79203933+pedrowlucas@users.noreply.github.com>
2022-07-27 12:17:07 -0300

    preparando terreno para validações

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-function limpar(){
-    var nome = document.getElementsByName('nome').value
-    var email = document.getElementsByName('email').value
-    var senha = document.getElementsByName('senha').value
-    var confSenha = document.getElementsByName('confSenha').value
+//Variáveis Globais
+var nome = document.getElementById('nome')
+var email = document.getElementById('email')
+var senha = document.getElementById('senha')
+var confSenha = document.getElementById('confSenha')
 
-    nome = "";
-    email = "";
-    senha = "";
-    confSenha = "";
-    console.log('Dados Limpados!')
+//Funções Auxiliares
+
+//Funções Principais
+function limpar(){
+    nome.value = "";
+    email.value = "";
+    senha.value = "";
+    confSenha.value = "";
+    alert('Dados Limpados!')
 }
 
 function validar(){
-    var nome = document.getElementsByName('nome').value
-    var email = document.getElementsByName('email').value
-    var senha = document.getElementsByName('senha').value
-    var confSenha = document.getElementsByName('confSenha').value
+    var name = nome.value
+    var mail = email.value
+    var psw = senha.value
+    var autPsw = confSenha.value
 
-    var resp = document.getElementsById('resp')
-    resp.innerHTML = `Olá ${nome}, email: ${email} e senha ${senha}`
+    if(name.lenght == 0 && mail.lenght == 0 && psw == 0 && autPsw == 0){
+        alert('Todos os dados devem ser preenchidos!')
+    }else if(name.lenght == 0){
+        alert('Preencha o seu nome!')
+    }else if(mail.lenght == 0){
+        alert('Preencha o seu email!')
+    }else if(psw.lenght == 0){
+        alert('Preencha o campo de senha!')
+    }else if(autPsw.lenght == 0){
+        alert('Por favor! Confirme sua senha')
+    }else{
+        confirm('Tudo certo!')
+
+    }
 }
