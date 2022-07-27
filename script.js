999fdd8dc826a9ba8731cdd454eda964d79554f5
Pedro Lucas <79203933+pedrowlucas@users.noreply.github.com>
2022-07-26 21:41:36 -0300

    criando os arquivos do projeto

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function limpar(){
-    var nome = document.getElementsByName('nome')
-    var email = document.getElementsByName('email')
-    var senha = document.getElementsByName('senha')
-    var confSenha = document.getElementsByName('confSenha')
+    var nome = document.getElementsByName('nome').value
+    var email = document.getElementsByName('email').value
+    var senha = document.getElementsByName('senha').value
+    var confSenha = document.getElementsByName('confSenha').value
+
+    nome = "";
+    email = "";
+    senha = "";
+    confSenha = "";
+    console.log('Dados Limpados!')
+}
+
+function validar(){
+    var nome = document.getElementsByName('nome').value
+    var email = document.getElementsByName('email').value
+    var senha = document.getElementsByName('senha').value
+    var confSenha = document.getElementsByName('confSenha').value
+
+    var resp = document.getElementsById('resp')
+    resp.innerHTML = `Olá ${nome}, email: ${email} e senha ${senha}`
 }
