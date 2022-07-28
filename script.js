ff0411b32e5a0fe629794cf4f5d0c08bbc5a394a
Pedro Lucas <79203933+pedrowlucas@users.noreply.github.com>
2022-07-27 21:02:34 -0300

    reformulando projeto

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 //Variáveis Globais
-var nome = document.getElementById('nome')
-var email = document.getElementById('email')
-var senha = document.getElementById('senha')
-var confSenha = document.getElementById('confSenha')
-
-//Funções Auxiliares
+const form = document.getElementById('form')
+const nome = document.getElementById('name')
+const email = document.getElementById('mail')
+const senha = document.getElementById('psw')
+const confSenha = document.getElementById('autPsw')
 
 //Funções Principais
-function limpar(){
-    nome.value = "";
-    email.value = "";
-    senha.value = "";
-    confSenha.value = "";
-    alert('Dados Limpados!')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()//Previnir o comportamento padrão dos browsers para formulário
+
+    checkInputs()
+})
+
+function checkInputs(){
+    const nameValue = nome.value.trim()//Função 'trim' é utilizada para remover os espaços no início e fim de uma string
+    const mailValue = email.value.trim()
+    const pswValue = senha.value.trim()
+    const autPswValue = confSenha.value.trim()
+
+    if(nameValue === '') {
+        //Adicionar classe de Erro
+        errorValidation(nome, 'Preencha esse campo')
+    } else {
+        //Adicionar classe de Sucesso
+        successValidation(nome)
+    }
+
+    if(mailValue === '') {
+        errorValidation(email, 'Preencha esse campo')
+    } else {
+        successValidation(email)
+    }
+
+    if(pswValue === ''){
+        errorValidation(senha, 'Preencha sua senha')
+    } else if(pswValue.length < 8){
+        errorValidation(senha, 'A senha deverá conter mais de 8 caracteres')
+    } else {
+        successValidation(senha)
+    }
+
+    if(autPswValue === '') {
+        errorValidation(confSenha, 'Preencha sua senha')
+    } else if(pswValue !== autPswValue){
+        errorValidation(confSenha, 'As senhas devem ser iguais')
+    } else {
+        successValidation(confSenha)
+    }
 }
 
-function validar(){
-    var name = nome.value
-    var mail = email.value
-    var psw = senha.value
-    var autPsw = confSenha.value
+//Funções Auxiliares
+function errorValidation(input, message){
+    const formControl = input.parentElement;//Retornando o elemento pai do input
+    const small = formControl.querySelector('small')
+    
+    small.innerText = message
+    formControl.className = 'form-control error'
+}
 
-    if(name.lenght == 0 && mail.lenght == 0 && psw == 0 && autPsw == 0){
-        alert('Todos os dados devem ser preenchidos!')
-    }else if(name.lenght == 0){
-        alert('Preencha o seu nome!')
-    }else if(mail.lenght == 0){
-        alert('Preencha o seu email!')
-    }else if(psw.lenght == 0){
-        alert('Preencha o campo de senha!')
-    }else if(autPsw.lenght == 0){
-        alert('Por favor! Confirme sua senha')
-    }else{
-        confirm('Tudo certo!')
+function successValidation(input){
+    const formControl = input.parentElement;
 
-    }
+    formControl.className = 'form-control success'
 }
